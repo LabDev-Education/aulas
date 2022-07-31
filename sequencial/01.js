c024894903f20fdf87e6c492e391d3db9a533c3b
Rai Vitor <raivitor@gmail.com>
2022-07-31 19:13:53 -0300

    primeiras 2 aulas

---
+++ sequencial/01.js
@@ -0,0 +1,12 @@
+/* Faça um programa que receba quatro números inteiros, 
+calcule e mostre a soma desses números.
+*/
+
+(function (){
+    const num1 = 10;
+    const num2 = 20;
+    const num3 = 30;
+    const num4 = 40;
+    const soma = num1 + num2 + num3 + num4;
+    console.log("A soma dos números é: " + soma);
+})()
---
+++ sequencial/02.js
@@ -0,0 +1,9 @@
+// Faça um programa que receba três notas, calcule e mostre a média aritmética.
+
+(function (){
+    const nota1 = 10;
+    const nota2 = 20;
+    const nota3 = 30;
+    const media = (nota1 + nota2 + nota3) / 3;
+    console.log("A média aritmética é: " + media);
+})()
